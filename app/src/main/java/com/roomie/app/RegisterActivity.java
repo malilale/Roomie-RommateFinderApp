1e5ca78b849cb54a30193631e39337f272fa68b8
Muhammed Ali LALE <malilale66@gmail.com>
2023-05-17 00:01:01 +0300

    email verification, password recovery and loading datas to firestore

--- app/src/main/java/com/roomie/app/RegisterActivity.java
+++ app/src/main/java/com/roomie/app/RegisterActivity.java
@@ -3,6 +3,7 @@ package com.roomie.app;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.widget.Button;
@@ -11,14 +12,20 @@ import android.widget.PopupMenu;
 import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
 
-import java.util.Objects;
+import java.util.HashMap;
+import java.util.Map;
 
 public class RegisterActivity extends AppCompatActivity {
 
     private EditText et_name, et_department, et_grade, et_state, et_distance, et_time, et_email, et_password;
     private FirebaseAuth mAuth;
     ProgressDialog progressDialog;
+    private FirebaseFirestore db = FirebaseFirestore.getInstance();
+    private DocumentReference documentReference;
 
 
     @Override
@@ -54,6 +61,7 @@ public class RegisterActivity extends AppCompatActivity {
     }
 
     private void register() {
+
         String name = et_name.getText().toString().trim();
         String email = et_email.getText().toString().trim();
         String grade = et_grade.getText().toString().trim();
@@ -78,16 +86,48 @@ public class RegisterActivity extends AppCompatActivity {
                         uploadData();
                     }else{
                         Toast.makeText(RegisterActivity.this, R.string.register_unsuccess, Toast.LENGTH_SHORT).show();
+                        progressDialog.dismiss();
                     }
                 });
             }else {
                 Toast.makeText(RegisterActivity.this, R.string.register_unsuccess, Toast.LENGTH_SHORT).show();
+                progressDialog.dismiss();
             }
         });
-        progressDialog.dismiss();
+
     }
 
     private void uploadData() {
+        FirebaseUser currentUser = FirebaseAuth.getInstance().getCurrentUser();
+        //create document
+        if(currentUser!=null)
+            documentReference = db.collection("Users").document(currentUser.getUid());
+
+        Map<String, String> user = new HashMap<>();
+        user.put("name", et_name.getText().toString().trim());
+        user.put("department", et_department.getText().toString().trim());
+        user.put("grade", et_grade.getText().toString().trim());
+        user.put("state", et_state.getText().toString().trim());
+        user.put("email", et_email.getText().toString().trim());
+        user.put("distance", et_distance.getText().toString().trim());
+        user.put("time", et_time.getText().toString().trim());
+        user.put("tel", "");
+        user.put("uid",currentUser.getUid());
+
+        documentReference.set(user)
+                .addOnSuccessListener(unused -> {
+                    Toast.makeText(RegisterActivity.this, R.string.save_success, Toast.LENGTH_SHORT).show();
+                    progressDialog.dismiss();
+                    sendToMain();})
+                .addOnFailureListener(e -> {
+                    Toast.makeText(RegisterActivity.this, R.string.save_unsuccess, Toast.LENGTH_SHORT).show();
+                    progressDialog.dismiss();});
+    }
+
+    private void sendToMain() {
+        Intent intent = new Intent(RegisterActivity.this, MainActivity.class);
+        startActivity(intent);
+        finish();
     }
 
 
